0239b623a292318df21855b2e08cc8735f329152
Alex Long <alexlong2001@outlook.com>
2019-05-24 18:50:35 +0100

    Animation removed, footer changed

--- src/js/script.js
+++ src/js/script.js
@@ -5,11 +5,3 @@ const servicesBox = document.getElementsByClassName('servicesColumn');
 navBarToggle.addEventListener('click', function() {
   mainNav.classList.toggle('pressed');
 });
-
-window.addEventListener('scroll', () => {
-  if (window.pageYOffset > 200) {
-    servicesBox[2].style.animation = '1s ease-in slide forwards';
-    servicesBox[1].style.animation = '1.3s ease-in slide forwards';
-    servicesBox[0].style.animation = '1.6s ease-in slide forwards';
-  }
-});
