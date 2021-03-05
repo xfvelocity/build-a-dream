800ee9a9e4536838f2e2eed42ffb2d1ef310f287
Alex Long <alexlong2001@outlook.com>
2021-03-05 18:09:43 +0000

    nuxt init

---
+++ pages/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <v-app>
+    <v-card>
+      <h1>Test</h1>
+    </v-card>
+  </v-app>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from "nuxt-property-decorator";
+
+@Component
+export default class index extends Vue {}
+</script>
