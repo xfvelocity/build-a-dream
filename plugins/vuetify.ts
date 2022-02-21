e9d49a31e28df541c2002ba4b0280cb5f39fb522
Alex Long <alexlong2001@outlook.com>
2022-02-21 19:34:44 +0000

    sorted colours

--- plugins/vuetify.ts
+++ plugins/vuetify.ts
@@ -2,15 +2,28 @@ import "@mdi/font/css/materialdesignicons.css";
 import "@vuetify/nightly/lib/styles/main.sass";
 
 import { defineNuxtPlugin } from "#app";
-import { createVuetify } from "@vuetify/nightly";
+import { createVuetify, ThemeDefinition } from "@vuetify/nightly";
 import * as components from "@vuetify/nightly/lib/components/index";
 import * as directives from "@vuetify/nightly/lib/directives/index";
+
+const customTheme: ThemeDefinition = {
+  dark: false,
+  colors: {
+    primary: "#086500",
+  },
+};
 
 export default defineNuxtPlugin((nuxtApp) => {
   nuxtApp.vueApp.use(
     createVuetify({
       components,
       directives,
+      theme: {
+        defaultTheme: "customTheme",
+        themes: {
+          customTheme,
+        },
+      },
     })
   );
 });
