abc760417bdb1f44d00891021d37988ade2bee1a
Alex Long <alexlong2001@outlook.com>
2022-04-05 08:17:59 +0100

    added google analytics

---
+++ plugins/gtag.ts
@@ -0,0 +1,8 @@
+import { defineNuxtPlugin } from "#app";
+import VueGtag from "vue-gtag";
+
+export default defineNuxtPlugin((nuxtApp) => {
+  nuxtApp.vueApp.use(VueGtag, {
+    config: { id: "G-JMLBDBBEE0" },
+  });
+});
