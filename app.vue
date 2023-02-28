029c0686ef19de038ad1464c7d0fce3e87b6336d
Alex Long <alex@feeditback.com>
2023-02-28 07:56:40 +0000

    init

---
+++ app.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <xf-nav> Test </xf-nav>
+
+    <nuxt-page />
+  </div>
+</template>
+
+<script type="ts" setup>
+import { XfNav } from 'xf-cmpt-lib'
+</script>
+
+<style lang="scss">
+@import "xf-cmpt-lib/dist/cmpt-lib-ts.css";
+</style>
