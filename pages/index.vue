4a469af284a9b277b0cf6ab9720995787d498fa7
Alex Long <alex@feeditback.com>
2023-03-02 07:35:08 +0000

    added services base

--- pages/index.vue
+++ pages/index.vue
@@ -23,7 +23,11 @@
 
   <!-- About me -->
   <div class="bd-about bd-max-width xf-p-4 xf-py-8">
-    <XfBanner class="xf-mx-auto xf-my-4" background-colour="primary">
+    <XfBanner
+      class="xf-mx-auto xf-my-4"
+      :font-size="32"
+      background-colour="primary"
+    >
       About us
     </XfBanner>
 
@@ -39,10 +43,73 @@
 
     <img class="xf-mt-6 xf-w-100" src="~/assets/images/logo-big.png" />
   </div>
+
+  <!-- Services -->
+  <div class="bd-services xf-px-4 xf-py-6">
+    <XfBanner
+      class="xf-mx-auto xf-mt-4 xf-mb-6"
+      :font-size="32"
+      background-colour="white"
+      text-colour="primary"
+    >
+      Services
+    </XfBanner>
+
+    <div class="bd-max-width xf-flex xf-flex-wrap xf-gap-3">
+      <div
+        v-for="(service, i) in services"
+        :key="i"
+        class="bd-services-item xf-p-4"
+      >
+        <XfIcon
+          :class="{ 'xf-flex-order-1': i % 2 === 0 }"
+          :src="getDynamicAsset(service.icon)"
+        />
+
+        <div :class="i % 2 === 0 ? 'xf-mr-4' : 'xf-ml-4'">
+          <h3 class="xf-text-colour-primary">{{ service.title }}</h3>
+          <p>{{ service.text }}</p>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script lang="ts" setup>
-import { XfButton, XfBanner } from "xf-cmpt-lib";
+import { XfButton, XfBanner, XfIcon } from "xf-cmpt-lib";
+
+const services = [
+  {
+    title: "Landscaping",
+    text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Placerat mi nulla massa.",
+    icon: "icons/tree.svg",
+  },
+  {
+    title: "Landscaping",
+    text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Placerat mi nulla massa.",
+    icon: "icons/tree.svg",
+  },
+  {
+    title: "Landscaping",
+    text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Placerat mi nulla massa.",
+    icon: "icons/tree.svg",
+  },
+  {
+    title: "Landscaping",
+    text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Placerat mi nulla massa.",
+    icon: "icons/tree.svg",
+  },
+  {
+    title: "Landscaping",
+    text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Placerat mi nulla massa.",
+    icon: "icons/tree.svg",
+  },
+  {
+    title: "Landscaping",
+    text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Placerat mi nulla massa.",
+    icon: "icons/tree.svg",
+  },
+];
 </script>
 
 <style lang="scss" scoped>
@@ -51,6 +118,21 @@ import { XfButton, XfBanner } from "xf-cmpt-lib";
     img {
       border: 1px solid black;
       border-radius: 20px;
+    }
+  }
+
+  &-services {
+    background: linear-gradient(
+      120.64deg,
+      rgba(8, 98, 0, 0.9) 16.11%,
+      #053f00 86.85%
+    );
+
+    &-item {
+      display: flex;
+      align-items: center;
+      background: white;
+      border-radius: 5px;
     }
   }
 }
