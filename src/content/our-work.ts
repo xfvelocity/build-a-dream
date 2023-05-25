b8b509a723f50e5b11675f17facdf79589e4d986
Alex Long <alexlong2001@outlook.com>
2023-05-25 07:02:56 +0100

    build fix

--- src/content/our-work.ts
+++ src/content/our-work.ts
@@ -1,6 +1,8 @@
+import HeaderImage from "../images/header.png";
+
 export const examplesOfWork = [
   {
-    img: "/src/images/header.png",
+    img: HeaderImage,
     location: "North Hykeham",
     completedDate: "17/05/2023",
     title: "Indian Sandstone Paving",
@@ -8,15 +10,10 @@ export const examplesOfWork = [
       "Our ever-popular Indian Sandstone paving slabs. All our paving work is laid on tarram, hardcore and a wet mortar base.",
       "Raised planters built using new pressure-treated sleepers with the addition of low voltage LED lights.",
     ],
-    additionalImages: [
-      "/src/images/header.png",
-      "/src/images/header.png",
-      "/src/images/header.png",
-      "/src/images/header.png",
-    ],
+    additionalImages: [HeaderImage, HeaderImage, HeaderImage, HeaderImage],
   },
   {
-    img: "/src/images/header.png",
+    img: HeaderImage,
     location: "North Hykeham",
     completedDate: "17/05/2023",
     title: "Indian Sandstone Paving",
@@ -24,15 +21,10 @@ export const examplesOfWork = [
       "Our ever-popular Indian Sandstone paving slabs. All our paving work is laid on tarram, hardcore and a wet mortar base.",
       "Raised planters built using new pressure-treated sleepers with the addition of low voltage LED lights.",
     ],
-    additionalImages: [
-      "/src/images/header.png",
-      "/src/images/header.png",
-      "/src/images/header.png",
-      "/src/images/header.png",
-    ],
+    additionalImages: [HeaderImage, HeaderImage, HeaderImage, HeaderImage],
   },
   {
-    img: "/src/images/header.png",
+    img: HeaderImage,
     location: "North Hykeham",
     completedDate: "17/05/2023",
     title: "Indian Sandstone Paving",
@@ -40,15 +32,10 @@ export const examplesOfWork = [
       "Our ever-popular Indian Sandstone paving slabs. All our paving work is laid on tarram, hardcore and a wet mortar base.",
       "Raised planters built using new pressure-treated sleepers with the addition of low voltage LED lights.",
     ],
-    additionalImages: [
-      "/src/images/header.png",
-      "/src/images/header.png",
-      "/src/images/header.png",
-      "/src/images/header.png",
-    ],
+    additionalImages: [HeaderImage, HeaderImage, HeaderImage, HeaderImage],
   },
   {
-    img: "/src/images/header.png",
+    img: HeaderImage,
     location: "North Hykeham",
     completedDate: "17/05/2023",
     title: "Indian Sandstone Paving",
@@ -56,11 +43,6 @@ export const examplesOfWork = [
       "Our ever-popular Indian Sandstone paving slabs. All our paving work is laid on tarram, hardcore and a wet mortar base.",
       "Raised planters built using new pressure-treated sleepers with the addition of low voltage LED lights.",
     ],
-    additionalImages: [
-      "/src/images/header.png",
-      "/src/images/header.png",
-      "/src/images/header.png",
-      "/src/images/header.png",
-    ],
+    additionalImages: [HeaderImage, HeaderImage, HeaderImage, HeaderImage],
   },
 ];
