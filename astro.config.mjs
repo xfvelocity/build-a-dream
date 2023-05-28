41ec99a215965f1657a4c246d6281bc5a8d1ff6e
Alex Long <alexlong2001@outlook.com>
2023-05-28 19:43:03 +0100

    clean up

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,10 @@
 import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
 
+import sitemap from "@astrojs/sitemap";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react()],
+  site: "https://buildadream.co.uk",
+  integrations: [react(), sitemap()],
 });
--- src/components/Pages/Home/Header/Header.tsx
+++ src/components/Pages/Home/Header/Header.tsx
@@ -1,32 +1,24 @@
 import React from "react";
 
-// Styles
-import "./Header.scss";
-
 // Components
-import HeaderCarousel from "./HeaderCarousel";
 import BdButton from "../../../Button/BdButton";
 
 const Header = () => {
   return (
-    <div className="header">
-      <HeaderCarousel />
+    <div className="header-text-container">
+      <div className="header-text xf-p-6 xf-p-lg-10">
+        <h1 className="xf-text-24 xf-text-40-xl xf-mb-2">
+          Dream gardens that don’t cost the earth
+        </h1>
+        <p className="xf-text-12 xf-text-14-lg xf-text-16-xl xf-fw-400 xf-mb-5 xf-mb-lg-7">
+          With competitive pricing, experience the value of our affordable
+          landscaping solutions as we create stunning outdoor spaces that suit
+          your needs and exceed your expectations.
+        </p>
 
-      <div className="header-text-container">
-        <div className="header-text xf-p-6 xf-p-lg-10">
-          <h1 className="xf-text-24 xf-text-40-xl xf-mb-2">
-            Dream gardens that don’t cost the earth
-          </h1>
-          <p className="xf-text-12 xf-text-14-lg xf-text-16-xl xf-fw-400 xf-mb-5 xf-mb-lg-7">
-            With competitive pricing, experience the value of our affordable
-            landscaping solutions as we create stunning outdoor spaces that suit
-            your needs and exceed your expectations.
-          </p>
-
-          <a href="/our-work">
-            <BdButton>View our work</BdButton>
-          </a>
-        </div>
+        <a href="/our-work">
+          <BdButton>View our work</BdButton>
+        </a>
       </div>
     </div>
   );
